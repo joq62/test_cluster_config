a538f20f86fbf0bc89da4b63b18352d159337812
joq62 <joakim.leche@gmail.com>
2021-01-11 22:25:47 +0100

    created

---
+++ cluster_info.hrl
@@ -0,0 +1,5 @@
+[db_passwd,"joq62","20Qazxsw20"].
+
+[db_server,"c0","joq62","festum01","192.168.0.200",22,not_available].
+[db_server,"c1","joq62","festum01","192.168.0.201",22,not_available].
+[db_server,"c2","joq62","festum01","192.168.0.202",22,not_available].
